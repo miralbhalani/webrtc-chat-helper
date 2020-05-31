6986a1b473948bb8bc4884060915940abe61176d
miral <miral.b@productivet.com>
2020-05-31 20:06:22 +0530

    b changes

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.abc = void 0;
 var ServerIO_1 = require("./ServerIO");
 Object.defineProperty(exports, "ServerIO", { enumerable: true, get: function () { return ServerIO_1.ServerIO; } });
+function abc() {
+    console.log('sdsd');
+}
+exports.abc = abc;
